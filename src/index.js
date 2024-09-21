4d3b53c4e946c2151e13009d27f0bf7f34860c03
Linttreen <jhuliegv@gmail.com>
2024-09-20 21:19:30 -0300

    chat

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const ws = require('ws')
-const server = new ws.Server({port: '3000'})
-
-server.on('connection', socket =>{
-    socket.on('message', message => {
-        console.log(message)
-        socket.send('${message}')
-    })
-})
