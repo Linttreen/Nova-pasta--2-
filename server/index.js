c878889eda10e42e2829c82331e16fb27eb2e7f5
Linttreen <jhuliegv@gmail.com>
2024-09-23 12:06:18 -0300

    terminando o chat

--- server/index.js
+++ server/index.js
@@ -1,10 +1,49 @@
-const ws = require('ws')
-const server = new ws.Server({ port: '3000' })
+import express from 'express'
+import { Server } from "socket.io"
+import path from 'path' 
+import { fileURLToPath } from 'url'
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        const b = Buffer.from(message)
-        console.log(message)
-        socket.send(`${message}`)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`)
+
+    //Upon connection - only to user
+    socket.emit('message', "Bem-vindo(a) ao chat!")
+
+    //Upon connection - to all others
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} entrou`)
+
+    //listening for a message event
+    socket.on('message', data => {
+        console.log(data)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    //when a user disconnects - to all others
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} saiu`)
+    })
+
+    //Listen for activivty
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })
